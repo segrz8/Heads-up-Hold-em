326428c2618b1ac99d401f65735c70f73b5e24ba
Sebastian Grzelak <segrz8@gmail.com>
2019-10-25 14:11:07 +0200

    Updates

---
+++ precache-manifest.5b0a72179a1103f931f917d113b77b35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80d54e1a8783d15fac69a4594f8d5fc9",
+    "url": "/Heads-up-Hold-em/index.html"
+  },
+  {
+    "revision": "90872f96efa55720376b",
+    "url": "/Heads-up-Hold-em/static/css/main.390d57eb.chunk.css"
+  },
+  {
+    "revision": "71a58432d3d9a543d4d3",
+    "url": "/Heads-up-Hold-em/static/js/2.f227c4ad.chunk.js"
+  },
+  {
+    "revision": "90872f96efa55720376b",
+    "url": "/Heads-up-Hold-em/static/js/main.82c0206d.chunk.js"
+  },
+  {
+    "revision": "05ad7ad5ce912a276b3a",
+    "url": "/Heads-up-Hold-em/static/js/runtime~main.3991e70c.js"
+  }
+]);
