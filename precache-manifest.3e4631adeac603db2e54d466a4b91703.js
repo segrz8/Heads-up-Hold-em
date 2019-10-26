76e5880bcb2ad050369bd436fea38d6e77307fc5
Sebastian Grzelak <segrz8@gmail.com>
2019-10-26 14:42:46 +0200

    Updates

--- precache-manifest.3e4631adeac603db2e54d466a4b91703.js
+++ precache-manifest.3e4631adeac603db2e54d466a4b91703.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dcafe4b3ca1fc8e55ff59496fc3060bd",
+    "revision": "33bc6110bece4d35d7fa9896f87b647a",
     "url": "/Heads-up-Hold-em/index.html"
   },
   {
-    "revision": "ea5d010d8492b580db70",
-    "url": "/Heads-up-Hold-em/static/css/main.32153c05.chunk.css"
+    "revision": "235a5422eb4bea2014af",
+    "url": "/Heads-up-Hold-em/static/css/main.e82e81c4.chunk.css"
   },
   {
     "revision": "71a58432d3d9a543d4d3",
     "url": "/Heads-up-Hold-em/static/js/2.f227c4ad.chunk.js"
   },
   {
-    "revision": "ea5d010d8492b580db70",
-    "url": "/Heads-up-Hold-em/static/js/main.82c0206d.chunk.js"
+    "revision": "235a5422eb4bea2014af",
+    "url": "/Heads-up-Hold-em/static/js/main.ea618b33.chunk.js"
   },
   {
     "revision": "05ad7ad5ce912a276b3a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Heads-up-Hold-em/precache-manifest.a975dcfb9c44033195bc982d6943ce72.js"
+  "/Heads-up-Hold-em/precache-manifest.3e4631adeac603db2e54d466a4b91703.js"
 );
 
 self.addEventListener('message', (event) => {
