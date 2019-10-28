9761e2b5aeafe3de0561dd7b25cf22a76131ba89
Sebastian Grzelak <segrz8@gmail.com>
2019-10-28 23:41:35 +0100

    delete cpu turn button

--- src/components/OperatingButtons.js
+++ src/components/OperatingButtons.js
@@ -2,11 +2,11 @@ import React from 'react'
 import './OperatingButtons.css';
 
 function OperatingButtons(props) {
-    const { dealCards, disabledDeal, player2turn, disabledPlayer2, showCards, disabledShowdown, player1money, player2money } = props
+    const { dealCards, disabledDeal, showCards, disabledShowdown } = props
     return (
         <div className='operatingButtons'>
             <button onClick={dealCards} disabled={disabledDeal}>Deal Cards</button>
-            <button onClick={player2turn} disabled={player1money === 0 || player2money === 0 ? true : disabledPlayer2}>CPU turn</button>
+            {/* <button onClick={player2turn} disabled={player1money === 0 || player2money === 0 ? true : disabledPlayer2}>CPU turn</button> */}
             <button onClick={showCards} disabled={disabledShowdown}>Showdown</button>
         </div>
     )
