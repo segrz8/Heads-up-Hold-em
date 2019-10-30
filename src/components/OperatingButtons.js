612e0fdbca63e2d9ae69c889c748c6b1578e2aef
Sebastian Grzelak <segrz8@gmail.com>
2019-10-30 23:59:48 +0100

    new game + new hand in only one button

--- src/components/OperatingButtons.js
+++ src/components/OperatingButtons.js
@@ -5,9 +5,13 @@ function OperatingButtons(props) {
     const { dealCards, disabledDeal, showCards, disabledShowdown } = props
     return (
         <div className='operatingButtons'>
-            <button onClick={dealCards} disabled={disabledDeal}>New Game</button>
+            {/* <button onClick={dealCards} disabled={disabledDeal}>New Game</button> */}
             {/* <button onClick={player2turn} disabled={player1money === 0 || player2money === 0 ? true : disabledPlayer2}>CPU turn</button> */}
-            <button onClick={showCards} disabled={disabledShowdown}>New hand</button>
+            {/* <button onClick={showCards} disabled={disabledShowdown}>New hand</button> */}
+            {disabledDeal ?
+                <button onClick={showCards} disabled={disabledShowdown}>New hand</button> :
+                <button onClick={dealCards} disabled={disabledDeal}>New Game</button>
+            }
         </div>
     )
 }
